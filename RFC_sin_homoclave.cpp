f3aff6385be13a21baa016ec76c14d03f1043495
Alain0307 <113079684+Alain0307@users.noreply.github.com>
2022-11-30 12:40:21 -0600

    Primer commit-segunda rama
    agregue espacios y puse más "bonito" el código

--- RFC_sin_homoclave.cpp
+++ RFC_sin_homoclave.cpp
@@ -13,16 +13,20 @@ int main()
 
 	printf("Nombre:");
 	scanf_s("%s", nombre, 20);
+	
 	printf("Apellido paterno:");
 	scanf_s("%s", app, 20);
+	
 	printf("Apellido materno:");
 	scanf_s("%s", apm, 20);
 
-	printf("Día de nacimiento:");
+	printf("Dia de nacimiento:");
 	scanf_s("%i", &dian);
+	
 	printf("Mes de nacimiento:");
 	scanf_s("%i", &mesn);
-	printf("Año de nacimiento:");
+	
+	printf("Anio de nacimiento:");
 	scanf_s("%i", &anon);
 
 	sprintf_s(sanon, "%4i", anon);
@@ -34,6 +38,7 @@ int main()
 	rfc[4] = sanon[2];
 	rfc[5] = sanon[3];
 	rfc[6] = '\0';
+	
 	printf("%s%02i%02i", rfc, mesn, dian);
 
 
